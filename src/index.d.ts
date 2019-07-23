c3f251ac1796ca23eb7bde9a319a2ccd7dea53c2
steven <guozhaolong@gmail.com>
2019-07-23 16:03:13 +0800

    增加命令栏提示

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,12 @@
 import * as React from 'react';
 export interface IDesignerProps {
-    onSave?: (d: Object) => void;
     data: Object;
+    height: Number;
+    mode: String;
+    lang: String;
+    isView: Boolean;
+    users: Array<Object>;
+    groups: Array<Object>;
 }
 
 export default class Designer extends React.Component<IDesignerProps, any> {}
